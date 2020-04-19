05231ed7a944574eb54951c0f94040f6d1fdc5c7
Khushk <hasham.khushk@intel.com>
2020-04-19 18:14:01 -0500

    Configured the deployment with DEBIAN_FRONTEND=noninteractive option in apt-get

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -8,8 +8,8 @@ PROJECT_GIT_URL='https://github.com/CHANGEME.git'
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
-apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get DEBIAN_FRONTEND=noninteractive update
+apt-get DEBIAN_FRONTEND=noninteractive install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
