3a9dde25ee0c891e7aed00b91cad59e001d605b4
Khushk <hasham.khushk@intel.com>
2020-04-20 11:03:52 -0500

    fixed a typo in permissions.py

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -5,7 +5,7 @@ class UpdateOwnProfile(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         '''Check user is trying to edit their own profile'''
-        if request.method in permissions.SAFEMETHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.id == request.user.id
@@ -15,7 +15,7 @@ class UpdateOwnStatus(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         '''Check user is trying to edit their own status'''
-        if request.method in permissions.SAFEMETHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.user_profile.id == request.user.id
